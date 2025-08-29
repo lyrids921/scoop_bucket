01241088244c9df34f80f597bc512b06cb803bd7
lyrids921 <lyrids921@outlook.com>
2025-08-29 23:36:32 +0800

    .

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "lyrids921/scoop_buckets:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
